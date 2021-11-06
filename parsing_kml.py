dde18ef7d240a368cbd0e26a9f7eb3d7adaefc8b
hafsaG <you@example.com>
2021-11-06 19:28:44 -0400

    parse

--- parsing_kml.py
+++ parsing_kml.py
@@ -1,13 +1,16 @@
 from pykml import parser
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 # Path to data folder
-adnan_path  = "C:/Users/hafsagureye/Downloads/HOhio/"
+adnan_path  = '/Users/hafsagureye/Documents/HOhio/'
 
 # Extract Transformers.kmz to file Transformers (I used 7Zip), inside should be doc.kml
-filename = adnan_path + 'Poles /doc.kml'
+filename = adnan_path + 'Poles/doc.kml'
 
-
+# /Users/hafsagureye/Documents/HOhio
 
 with open(filename) as f:
     doc = parser.parse(f).getroot()
@@ -24,4 +27,43 @@ df = pd.DataFrame()
 df['latitude'] = lat
 df['longitude'] = long
 
+#latArr = []
+#longArr = []
+
+#latArr['latitude'] = lat
+#longArr['longitude'] = long
+
 print(df.head())
+
+#floatX = lat.astype(float)
+
+
+#floatY = long.astype(float)
+x = 0
+
+y = 0
+
+latF = float(lat[x])
+
+longF = float(long[y])
+  
+plt.ylabel('Latitiude')
+plt.xlabel('Longitude')
+plt.title('Latitude and Longitude')
+
+for x in latF:
+  for y in longF:
+    plt.scatter(lat[x], lat[y])
+
+#for i in longF:
+  #float(long[i])
+
+#plt.plot(df['latitude'], df['longitude'],'ro')
+#plt.scatter([latF], [longF])
+#plt.scatter(latArr, latArr)
+plt.xlim(-100, 10)
+plt.ylim(-100, 10)
+
+#plt.axis([0, 10], [0, 10])
+plt.grid(True)
+plt.show()
