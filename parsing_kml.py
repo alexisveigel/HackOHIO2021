4abfab8837807f6f88f6bac48f67fe75af4f8878
Adnan Reddy <adnanreddy@outlook.com>
2021-11-06 20:32:21 -0400

    csv

--- parsing_kml.py
+++ parsing_kml.py
@@ -1,14 +1,15 @@
 from pykml import parser
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 
 # Path to data folder
-adnan_path  = '/Users/kateherz/HackOHIO2021/'
+adnan_path  = 'C:/Users/adnanreddy/Downloads/HOhio/'
 
 # Extract Transformers.kmz to file Transformers (I used 7Zip), inside should be doc.kml
 poles = adnan_path + 'Poles/doc.kml'
 transformers = adnan_path + 'Transformers/doc.kml'
+conductors = adnan_path + 'Primary_Conductor/doc.kml'
 
 
 # /Users/hafsagureye/Documents/HOhio
@@ -35,22 +36,30 @@ for e in docT.Document.Folder.Placemark:
   latT.append(coor[0])
   longT.append(coor[1])
 
+# with open(conductors) as f:
+#   docC = parser.parse(f).getroot()
 
+# namesC = []
+# latC = []
+# longC = []
+# for e in docC.Document.Folder.Placemark:
+#   coor = e.Point.coordinates.text.split(',')
+#   latC.append(coor[0])
+#   longC.append(coor[1])
 
+# plt.title("Latitude and Longtitude")  # Add a title to the axes.
+# plt.ylabel('Latitude')
+# plt.xlabel('Longtitude')
+# plt.grid(True)
+# plt.axis()
 
-plt.title("Latitude and Longtitude")  # Add a title to the axes.
-plt.ylabel('Latitude')
-plt.xlabel('Longtitude')
-plt.grid(True)
-plt.axis()
-
-lat_nums = []
+lat_numsP = []
 for x in latP:
-  lat_nums.append(float(x))
+  lat_numsP.append(float(x))
 
-long_nums = []
+long_numsP = []
 for x in longP:
-  long_nums.append(float(x))
+  long_numsP.append(float(x))
 
 lat_numsT = []
 for x in latT:
@@ -60,7 +69,31 @@ long_numsT = []
 for x in longT:
   long_numsT.append(float(x))
 
-plt.scatter(lat_nums,long_nums,.5)
-plt.scatter(lat_numsT,long_numsT,.5)
+# lat_numsC = []
+# for x in latC:
+#   lat_numsC.append(float(x))
 
-plt.show()
+# long_numsC = []
+# for x in longC:
+#   long_numsC.append(float(x))
+
+# plt.scatter(lat_nums,long_nums,.5)
+# plt.scatter(lat_numsT,long_numsT,.5)
+
+# plt.show()
+
+df_p = pd.DataFrame()
+df_p['Latitude'] = lat_numsP
+df_p['Longitude'] = long_numsP
+
+df_t = pd.DataFrame()
+df_t['Latitude'] = lat_numsP
+df_t['Longitude'] = long_numsP
+
+# df_c = pd.DataFrame()
+# df_c['Latitude'] = lat_numsC
+# df_c['Longitude'] = long_numsC
+
+df_p.to_csv(path_or_buf='poles.csv')
+df_t.to_csv(path_or_buf='transformers.csv')
+# df_c.to_csv(path_or_buf='conductors.csv')
